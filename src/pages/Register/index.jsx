d416f526174deb9c064edbd4f11c52bff0302d03
esterfrazao <tafnisbonbon@gmail.com>
2022-09-22 13:37:02 -0300

    feat: add register page form

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,5 +1,78 @@
+import {
+  Box,
+  Flex,
+  Heading,
+  Link as Span,
+  Stack,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import Form from "../../components/Form";
+import { registerSchema } from "../../schemas/yupSchemas";
+
 const Register = () => {
-  return <p>Register</p>;
+  const fields = [
+    {
+      id: "name",
+      name: "name",
+      title: "Nome Completo",
+      type: "text",
+      placeholder: "Insira seu nome completo",
+    },
+    {
+      id: "phoneNumber",
+      name: "phone_number",
+      title: "Número de Telefone",
+      type: "text",
+      placeholder: "(86) 99999-9999",
+    },
+    {
+      id: "email",
+      name: "email",
+      title: "Email",
+      type: "email",
+      placeholder: "Insira seu email",
+    },
+    {
+      id: "password",
+      name: "password",
+      title: "Senha",
+      type: "password",
+      placeholder: "Insira sua senha",
+    },
+  ];
+
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"lg"} py={5} px={6}>
+        <Heading fontSize={"4xl"} textAlign={"center"}>
+          Cadastro
+        </Heading>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={10}
+        >
+          <Form schema={registerSchema} fields={fields} />
+          <Stack pt={6}>
+            <Text align={"center"}>
+              Já possui uma conta?
+              <Span color="var(--light-blue)">
+                <Link to="/">Faça Login</Link>
+              </Span>
+            </Text>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
 };
 
 export default Register;
--- src/schemas/yupSchemas.js
+++ src/schemas/yupSchemas.js
@@ -4,3 +4,13 @@ export const loginSchema = yup.object().shape({
   email: yup.string().email("Email Inválido").required("Campo Obrigatório"),
   password: yup.string().required("Campo Obrigatório"),
 });
+
+export const registerSchema = yup.object().shape({
+  name: yup.string().required("Campo Obrigatório"),
+  email: yup.string().email("Email Inválido").required("Campo Obrigatório"),
+  password: yup.string().required("Campo Obrigatório"),
+  phone_number: yup
+    .string()
+    .required("Campo Obrigatório")
+    .max(15, "Número muito longo"),
+});
