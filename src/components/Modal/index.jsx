d04630738b472c127598e65228575269b24927c2
esterfrazao <tafnisbonbon@gmail.com>
2022-09-23 13:30:37 -0300

    feta: add modal component

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,39 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+} from "@chakra-ui/react";
+
+import Form from "../Form";
+
+const CustomModal = ({ isOpen, onOpen, onClose, form }) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Modal Title</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Form
+            fields={form.fields}
+            schema={form.schema}
+            callback={form.callback}
+          />
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CustomModal;
